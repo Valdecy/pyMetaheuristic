c1e798cd647724d414f3622400c5c42e8ed7cd0a
Valdecy Pereira <valdecypereira@yahoo.com.br>
2021-03-15 10:24:31 -0300

    Add files via upload

---
+++ pyMetaheuristic/dispersive_flies_optimization/dfo.py
@@ -0,0 +1,62 @@
+############################################################################
+
+# Created by: Prof. Valdecy Pereira, D.Sc.
+# UFF - Universidade Federal Fluminense (Brazil)
+# email:  valdecy.pereira@gmail.com
+# Course: Metaheuristics
+# Lesson: Dispersive Flies Optimization
+
+# Citation: 
+# PEREIRA, V. (2018). Project: Metaheuristic-Dispersive_Flies_Optimization, File: Python-MH-Dispersive Flies Optimization.py, GitHub repository: <https://github.com/Valdecy/Metaheuristic-Dispersive_Flies_Optimization>
+
+############################################################################
+
+# Required Libraries
+import math
+import numpy  as np
+import os
+
+# Function
+def target_function():
+    return
+
+# Function: Initialize Variables
+def initial_flies(swarm_size = 3, min_values = [-5,-5], max_values = [5,5], target_function = target_function):
+    position = np.zeros((swarm_size, len(min_values)+1))
+    for i in range(0, swarm_size):
+        for j in range(0, len(min_values)):
+             r = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+             position[i,j] = min_values[j] + r*(max_values[j] - min_values[j])
+        position[i,-1] = target_function(position[i,0:position.shape[1]-1])
+    return position
+
+# Function: Update Position
+def update_position(position, neighbour_best, swarm_best, min_values = [-5,-5], max_values = [5,5], fly = 0, target_function = target_function):
+    for j in range(0, position.shape[1] - 1):
+        r = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+        position[fly, j] = np.clip((neighbour_best[j] + r*(swarm_best[j] - position[fly, j])), min_values[j], max_values[j])
+    position[fly, -1] = target_function(position[fly, 0:position.shape[1]-1])
+    return position
+
+# DFO Function
+def dispersive_fly_optimization(swarm_size = 3, min_values = [-5,-5], max_values = [5,5], generations = 50, dt = 0.2, target_function = target_function):
+    count = 0
+    population = initial_flies(swarm_size = swarm_size, min_values = min_values, max_values = max_values, target_function = target_function)
+    neighbour_best = np.copy(population[population[:,-1].argsort()][0,:])
+    swarm_best     = np.copy(population[population[:,-1].argsort()][0,:])
+    while (count <= generations):
+        print("Generation: ", count, " of ", generations, " f(x) = ", swarm_best[-1])
+        for i in range (0, swarm_size):
+            population = update_position(population, neighbour_best, swarm_best, min_values = min_values, max_values = max_values, fly = i, target_function = target_function)
+            r = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+            if (r < dt):
+                for j in range(0, len(min_values)):
+                    r = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+                    population[i,j] = min_values[j] + r*(max_values[j] - min_values[j])
+                population[i,-1] = target_function(population[i,0:population.shape[1]-1])             
+        neighbour_best = np.copy(population[population[:,-1].argsort()][0,:])
+        if (swarm_best[-1] > neighbour_best[-1]):
+           swarm_best = np.copy(neighbour_best)
+        count = count + 1
+    print(swarm_best)
+    return swarm_best
