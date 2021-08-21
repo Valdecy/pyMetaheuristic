83e17a9bad3ad1100a9f7ba5b6dfca4e838edddc
Valdecy Pereira <valdecypereira@yahoo.com.br>
2021-08-21 11:08:39 -0300

    Update gwo.py

--- pyMetaheuristic/grey_wolf_optimizer/gwo.py
+++ pyMetaheuristic/grey_wolf_optimizer/gwo.py
@@ -106,6 +106,5 @@ def grey_wolf_optimizer(pack_size = 5, min_values = [-5,-5], max_values = [5,5],
         a_linear_component = 2 - count*(2/iterations)
         alpha, beta, delta = update_pack(position, alpha, beta, delta)
         position           = update_position(position, alpha, beta, delta, a_linear_component = a_linear_component, min_values = min_values, max_values = max_values, target_function = target_function)       
-        count              = count + 1       
-    print(alpha[-1])    
+        count              = count + 1          
     return alpha
