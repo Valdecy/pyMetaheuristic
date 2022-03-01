0476dee758425c52e17220fe4435ca638d0524d6
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-03-01 10:19:54 -0300

    Update single.py

--- pyMetaheuristic/test_function/single.py
+++ pyMetaheuristic/test_function/single.py
@@ -1,6 +1,6 @@
-from numpy import cos, exp, pi
+import numpy as np
 
-
+#
 def easom(variables_values: tuple[float, float]) -> float:
     x1, x2 = variables_values
-    return -cos(x1) * cos(x2) * exp(-(x1 - pi) ** 2 - (x2 - pi) ** 2)
+    return -np.cos(x1) * np.cos(x2) * np.exp(-(x1 - np.pi) ** 2 - (x2 - pi) ** 2)
