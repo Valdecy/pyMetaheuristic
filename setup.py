b3c0e42688b8078ede8bfcad20a93718dc55e34a
mraiser <mraiser@foxmail.com>
2022-03-01 15:29:50 +0800

    modularization.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyMetaheuristic",
+    version="0.0.1",
+    author="",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy'
+    ],
+)
