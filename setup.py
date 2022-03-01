6c9724eef90a7f2ab5bd974c4f5936d389877707
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-03-01 10:19:09 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyMetaheuristic",
-    version="0.0.1",
-    author="",
-    author_email="",
+    name='pyMetaheuristic',
+    version='1.0.1',
+    license='GNU',
+    author='Valdecy pereira',
+    author_email='valdecy.pereira@gmail.com',
+    url='https://github.com/Valdecy/pyMetaheuristic',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
