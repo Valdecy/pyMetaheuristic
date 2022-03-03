8309a8a24bf778903c8287298d96b6f06d43de1b
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-03-03 20:16:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyMetaheuristic',
-    version='1.0.5',
+    version='1.0.6',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
