ebf12e4947f48a37c5b7d01a5a2853cda0279109
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-03-05 17:50:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pyMetaheuristic',
@@ -12,4 +16,6 @@ setup(
         'numpy',
         'scipy'
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
