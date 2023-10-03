44dab5a4db6616a2aa07373815ae37608ea185dc
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-10-02 22:31:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pyMetaheuristic',
-    version='1.4.1',
+    version='1.4.2',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
